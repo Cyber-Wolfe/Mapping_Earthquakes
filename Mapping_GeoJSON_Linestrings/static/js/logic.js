56cb225164837bd8c919939cf3310dc11230209b
Joshua Wolfe <joshwolfe95@gmail.com>
2022-05-21 13:42:09 -0400

    airline routes

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -8,7 +8,7 @@ console.log("working");
 // 'satellite-streets-v11' to make it satellite look 
 // 13.4.1 for mapping documentation
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data @copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -22,31 +22,41 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Street: light,
   Dark: dark
 };
 
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [[44.0, -80.0]],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Cyber-Wolfe/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/Cyber-Wolfe/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h2>" + 'Airport Code: ' + feature.properties.faa + "</h2>"  + "<hr>" + "<h3>" + 'Airport Name: ' + feature.properties.name + "</h3>");
-  }
-}).addTo(map);
+L.geoJSON(data).addTo(map);
 });
+
+
+
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("<h2>" + 'Airport Code: ' + feature.properties.faa + "</h2>"  + "<hr>" + "<h3>" + 'Airport Name: ' + feature.properties.name + "</h3>");
+//   }
+// }).addTo(map);
+// });
